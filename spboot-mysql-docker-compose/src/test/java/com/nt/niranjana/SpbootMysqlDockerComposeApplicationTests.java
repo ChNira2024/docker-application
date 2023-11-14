9d78649d07fc85e7caaeeab9dc89f9a80710c22c
niranjanaC2023 <136884621+niranjanaC2023@users.noreply.github.com>
2023-11-14 19:00:00 +0530

    docker-mysql

---
+++ spboot-mysql-docker-compose/src/test/java/com/nt/niranjana/SpbootMysqlDockerComposeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nt.niranjana;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpbootMysqlDockerComposeApplicationTests {
+
+	
+	void contextLoads() {
+		String name="Raja";
+	}
+
+}
